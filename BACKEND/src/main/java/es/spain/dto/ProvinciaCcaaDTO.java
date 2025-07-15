10c31c0d689053fd0a4eb5e7beaad4c5f4af388a
PabloGonsan <117724841+PabloGonSan8@users.noreply.github.com>
2025-07-15 09:50:23 +0200

    feat: implement ProvinciaCcaaDTO class with properties and methods

--- BACKEND/src/main/java/es/spain/dto/ProvinciaCcaaDTO.java
+++ BACKEND/src/main/java/es/spain/dto/ProvinciaCcaaDTO.java
@@ -1,4 +1,28 @@
 package es.spain.dto;
 
 public class ProvinciaCcaaDTO {
+
+    private String provincia;
+    private String ccaa;
+
+    public ProvinciaCcaaDTO(String provincia, String ccaa) {
+        this.provincia = provincia;
+        this.ccaa = ccaa;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCcaa() {
+        return ccaa;
+    }
+
+    public void setCcaa(String ccaa) {
+        this.ccaa = ccaa;
+    }
 }
