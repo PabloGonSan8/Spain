4efcbed7e5909ac20cc6f36b0da6803e0b029268
PabloGonsan <117724841+PabloGonSan8@users.noreply.github.com>
2025-11-13 15:20:03 +0100

    .

--- BACKEND/src/main/java/es/spain/model/Provincia.java
+++ BACKEND/src/main/java/es/spain/model/Provincia.java
@@ -3,6 +3,7 @@ package es.spain.model;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 
 @Entity
@@ -10,15 +11,15 @@ import java.util.List;
 public class Provincia {
 
         @Id
-        @Column(name = "idProvincia")
+        @Column(name = "idprovincia")
         private Long idProvincia;
 
-        @Column(name = "Nombre", nullable = false, length = 50)
+        @Column(name = "nombre", nullable = false, length = 50)
         private String nombre;
 
         /* FK hacia CCAA */
         @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "codCcaa")
+        @JoinColumn(name = "codccaa")
         private Ccaa ccaa;
 
     /* Relación con Localidad */
@@ -55,5 +56,17 @@ public class Provincia {
 
     public void setCcaa(Ccaa ccaa) {
         this.ccaa = ccaa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Provincia provincia = (Provincia) o;
+        return Objects.equals(idProvincia, provincia.idProvincia) && Objects.equals(nombre, provincia.nombre) && Objects.equals(ccaa, provincia.ccaa) && Objects.equals(localidades, provincia.localidades);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idProvincia, nombre, ccaa, localidades);
     }
 }
